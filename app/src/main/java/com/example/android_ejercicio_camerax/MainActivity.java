3f706107f4666f56ea95b603a891d988c926faaf
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2023-01-09 16:10:29 +0100

    Solve bug

--- app/src/main/java/com/example/android_ejercicio_camerax/MainActivity.java
+++ app/src/main/java/com/example/android_ejercicio_camerax/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupListeners();
         openGallery = findViewById(R.id.openGallery);
+        setupListeners();
         this.someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
